48e0125ebf24bbf9f652580d6b4eacad43b2baf9
Arya Karimi <devaryakrm@gmail.com>
2023-06-26 15:17:47 +0300

    init

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Nav.scss";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <img src="/assets/fatoArttLogo.png" alt="Fato Artt Logo" />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/components/Sec1/Sec1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Sec1.scss";
+
+function Sec1() {
+  return (
+    <div className="Sec1">
+      <h1>Sevdikleriniz için en özel hediye. </h1>
+      <h2>
+        %100 el yapımı uygun fiyatlı karakalem çalışmaları için hemen şimdi
+        bizimle iletişime geçin!
+      </h2>
+      <div className="contact-icons"></div>
+    </div>
+  );
+}
+
+export default Sec1;
